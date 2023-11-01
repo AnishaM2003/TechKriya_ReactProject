7ce35418db2eb7b9713f5af0530bbde553d8127a
Anisha <anisha.madalemula@gmail.com>
2023-11-01 23:18:52 +0530

    react_project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechKriya</h1>
+            <p>All rights are reserved</p>
+        </div>
+        <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/the_anisha_m" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/AnishaM2003/" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
